1f1371ff6de8f06144dba20ae0506a9c7e13841e
Jonathan Shieh <jonathan.shieh@gmail.com>
2016-11-01 00:11:40 +0800

    Super basic version of read-structure ready

--- src/gd_edit/core.clj
+++ src/gd_edit/core.clj
@@ -2,9 +2,9 @@
   (:require [clojure.java.io :as io]
             [gloss.core :as gloss]
             [gloss.io])
-  (:import [java.nio.file Path Paths Files FileSystems]
-           [java.nio.file StandardOpenOption]
-           [java.nio.channels FileChannel])
+  (:import  [java.nio ByteBuffer]
+            [java.nio.file Path Paths Files FileSystems StandardOpenOption]
+            [java.nio.channels FileChannel])
   (:gen-class))
 
 
@@ -13,16 +13,64 @@
   [& args]
   (println "Hello, World!"))
 
+(def primitives-specs
+  ;; name => primitives spec
+  {:byte   [:byte    1 #(.get %)      ]
+   :int16  [:int16   2 #(.getShort %) ]
+   :int32  [:int     4 #(.getInt %)   ]
+   :float  [:float   4 #(.getFloat %) ]
+   :double [:double  4 #(.getDouble %)]
+   })
 
-(gloss/defcodec arz-header-codec
-  (gloss/ordered-map
-   :unknown :uint16-le
-   :version :uint16-le
-   :record-table-start   :uint32-le
-   :record-table-size    :uint32-le
-   :record-table-entries :uint32-le
-   :string-table-start   :uint32-le
-   :string-table-size    :uint32-le))
+(defn sizeof
+  "Given a spec, return the byte size it represents"
+  [spec]
+
+  ;; Look through all the items in the given spec
+  (reduce
+   (fn [accum item]
+     ;; Does a matching entry exist for the item in the primitives array?
+     (let [primitives-spec (primitives-specs item)]
+       (if primitives-spec
+         ;; If so, add the size to the accumulator
+         (+ accum (nth primitives-spec 1))
+
+         ;; If not, we've come across a field name. Don't do anything
+         accum
+         )))
+
+   ;; Start the sum at 0
+   0
+
+   ;; Ignore all odd fields, assuming they are fieldnames
+   (take-nth 2 (rest spec))))
+
+(defn read-struct
+  [spec ^ByteBuffer bb]
+
+  (reduce
+   (fn [accum item]
+     ;; Does a matching entry exist for the item in the primitives array?
+     (let [primitives-spec (primitives-specs item)]
+       (if primitives-spec
+         ;; Look up the associated function to retrieve the item from the byte buffer
+         ;; Run it with the supplied byte buffer and add that to the results
+         (conj accum ((nth primitives-spec 2) bb))
+
+         ;; If not, we've come across a field name. Place the fieldname into the result.
+         (conj accum item)
+         )))
+   [] spec))
+
+
+(def arz-header
+  [:unknown              :int16
+   :version              :int16
+   :record-table-start   :int32
+   :record-table-size    :int32
+   :record-table-entries :int32
+   :string-table-start   :int32
+   :string-table-size    :int32])
 
 ;; (gloss/defcodec arz-record-header-codec
 ;;   (gloss/ordered-map
@@ -30,12 +78,12 @@
 ;;    :count :uint16-le
 ;;    :fieldname-index   :uint32-le))
 
-(gloss/defcodec arz-string-table-codec
-  (gloss/repeated
-   (gloss/finite-frame
-    (gloss/prefix :uint32-le)
-    (gloss/string :ascii))
-   :prefix :uint32-le))
+;; (gloss/defcodec arz-string-table-codec
+;;   (gloss/repeated
+;;    (gloss/finite-frame
+;;     (gloss/prefix :uint32-le)
+;;     (gloss/string :ascii))
+;;    :prefix :uint32-le))
 
 
 (defn mmap
@@ -53,7 +101,7 @@
   
   ;; Jump to the start of the file header and start decoding
   (.position bb 0)
-  (gloss.io/decode arz-header-codec bb false))
+  (read-struct arz-header bb))
 
 
 ;; After some number of attempts to get gloss to load the string table, I gave up and just
@@ -114,6 +162,7 @@
 
 
 #_(def f (mmap "/Users/Odie/Dropbox/Public/GrimDawn/database/database.arz"))
+#_(.order f java.nio.ByteOrder/LITTLE_ENDIAN)
 #_(def h (load-db-header f))
 #_(.position f (:string-table-start h))
 #_(gloss.io/decode arz-string-table-codec f false)
