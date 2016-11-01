8da1ba445b5199c28173d416e8c394800c1da997
Jonathan Shieh <jonathan.shieh@gmail.com>
2016-11-01 11:45:51 +0800

    Now able to read the string table
    However, reading is extremely slow.
    A hand-coded version takes about 10ms to complete.
    The read-structure version takes 1s to complete.
    That's 100 times slower. Not really acceptable.

--- src/gd_edit/core.clj
+++ src/gd_edit/core.clj
@@ -13,6 +13,12 @@
   [& args]
   (println "Hello, World!"))
 
+(defn spec-type
+  [spec & rest]
+  (:struct/type (meta spec)))
+
+(defmulti read-spec spec-type)
+
 (def primitives-specs
   ;; name => primitives spec
   {:byte   [:byte    1 #(.get %)      ]
@@ -45,32 +51,216 @@
    ;; Ignore all odd fields, assuming they are fieldnames
    (take-nth 2 (rest spec))))
 
+(defn ordered-map?
+  [spec]
+  (= (-> spec
+         meta
+         :struct/return-type)
+     :map))
+
+(defn strip-orderedmap-fields
+  "Returns a spec with the keyname fields stripped if it looks like an orderedmap"
+  [spec]
+
+  ;; If it looks like an ordered map
+  (if (ordered-map? spec)
+
+    ;; strip all the odd number fields
+    (take-nth 2 (rest spec))
+
+    ;; Doesn't look like an ordered map...
+    ;; Return the spec untouched
+    spec))
+
 (defn read-struct
+  "Spec can also be a single primitive spec. In that case, we'll just read
+  a single item from the bytebuffer.
+
+  Spec can be a simple sequence of type specs. In that case, a list values
+  that corresponds to each item in the given spec.
+
+  Spec can be a collect with some special meta tags. In those cases, special
+  handling will be invoked via the 'read-spec' multimethod."
   [spec ^ByteBuffer bb]
 
-  (reduce
-   (fn [accum item]
-     ;; Does a matching entry exist for the item in the primitives array?
-     (let [primitives-spec (primitives-specs item)]
-       (if primitives-spec
-         ;; Look up the associated function to retrieve the item from the byte buffer
-         ;; Run it with the supplied byte buffer and add that to the results
-         (conj accum ((nth primitives-spec 2) bb))
+  ;; Handle single primitive case
+  ;; Handle sequence specs case
+  ;; Handle complicated case
 
-         ;; If not, we've come across a field name. Place the fieldname into the result.
-         (conj accum item)
-         )))
-   [] spec))
+  (let [;; Get a list of specs to read without the fieldnames
+        stripped-spec (strip-orderedmap-fields spec)
 
+        ;; Read in data using the list of specs
+        values (read-spec spec bb)]
+
+    ;; We've read all the values
+    ;; If the user didn't ask for a map back, then we're done
+    (if (not (ordered-map? spec))
+      values
+
+      ;; The user did ask for a map back
+      ;; Construct a map using the fieldnames in the spec as the key and the read value as the value
+      (zipmap (take-nth 2 spec) values))
+    ))
+
+
+(defmethod read-spec :default
+  [spec bb]
+
+  (if (seq? spec)
+    ;; If we're looking at some sequence, assume this is a sequence of specs and
+    ;; read through it all
+    (reduce
+     (fn [accum spec-item]
+       (conj accum (read-spec spec-item bb)))
+     []
+     spec)
+    
+    ;; Otherwise, we should be dealing with some kind of primitive...
+    ;; Look up the primitive keyword in the primitives map
+    (let [primitives-spec (primitives-specs spec)]
+
+      ;; If the primitive is found...
+      (if primitives-spec
+
+        ;; Execute the read function now
+        ((nth primitives-spec 2) bb)
+
+        ;; Otherwise, give up
+        (throw (Throwable. (str "Cannot handle spec:" spec)))))))
+
+
+(defn ordered-map
+  "Tags the given spec so read-structure will return a map instead of a vector.
+  Returns the collection it was given."
+  [& fields]
+  (with-meta fields {:struct/return-type :map}))
+
+(defn variable-count
+  "Tags the given spec to say it will repeat a number of times.
+
+  Exmaple:
+  (variable-count :uint32 :count-prefix :uint16)
+
+  This says we will read a :uint16 first to determine how many :uint32 to read
+  from the stream."
+  [spec & {:keys [length-prefix]
+           :or {length-prefix :int32}}]
+
+  ;; We're about to attach some meta info to the spec
+  ;; First, wrap the spec in another collection...
+  ;; We'll attach the information to information to that collection instead of the
+  ;; original spec
+  (let [spec-seq (list spec)]
+
+    ;; Attach the meta info to the spec 
+    ;; Any sequence can be have meta info attached
+    (with-meta spec-seq
+      {:struct/type :variable-count
+       :struct/length-prefix length-prefix})))
+
+
+(defmethod read-spec :variable-count
+  [spec bb]
+
+  (let [
+        ;; Destructure fields in the attached meta info
+        {length-prefix :struct/length-prefix} (meta spec)
+
+        ;; Read out the count of the spec to read
+        length (read-spec length-prefix bb)
+
+        ;; Unwrap the spec so we can read it
+        unwrapped-spec (first spec)
+        ]
+
+    ;; Read the spec "length" number of times and accumulate the result into a vector
+    ;; We're abusing reduce here by producing a lazy sequence to control the number of
+    ;; iterations we run.
+    (reduce
+     (fn [accum _]
+       ;; Read another one out
+       (conj accum (read-spec unwrapped-spec bb)))
+     []
+     (repeat length 1))))
+
+
+(defn string
+  "Returns a spec used to read a string of a specific type of encoding.
+  Can supply :length-prefix to indicate how to read the length of the string.
+  The default :length-prefix is a :int32."
+  [enc & {:keys [length-prefix]
+          :or {length-prefix :int32}}]
+
+  ;; Tag a dummy sequence with the info passed into the function
+  (with-meta '(:string)
+    {:struct/type :string
+     :struct/length-prefix length-prefix
+     :struct/string-encoding enc}))
+
+
+(defmethod read-spec :string
+  [spec bb]
+
+  (let [
+        valid-encodings {:ascii "US-ASCII"
+                         :utf-8 "UTF-8"}
+
+        ;; Destructure fields in the attached meta info
+        {length-prefix :struct/length-prefix requested-encoding :struct/string-encoding} (meta spec)
+
+        ;; Read out the length of the string
+        length (read-spec length-prefix bb)
+
+        ;; Create a temp buffer to hold the bytes before turning it into a java string
+        buffer (byte-array length)
+        ]
+
+    ;; Read the string bytes into the buffer
+    (.get bb buffer 0 length)
+
+    (String. buffer (valid-encodings requested-encoding))))
+
+
+(defn seq->bytes
+  [seq]
+
+  ;; Construct a byte array from the contents of...
+  (byte-array
+
+   ;; sequence of bytes reduced from individual items in the given sequence
+   (reduce
+    (fn [accum item]
+      (conj accum (byte item))) [] seq)
+   ))
+
+
+(defn bytes->bytebuffer
+  [bytes]
+
+  (ByteBuffer/wrap bytes))
+
+
+(defn seq->bytebuffer
+  [seq]
+  (-> seq
+      seq->bytes
+      bytes->bytebuffer))
 
 (def arz-header
-  [:unknown              :int16
+  (ordered-map 
+   :unknown              :int16
    :version              :int16
    :record-table-start   :int32
    :record-table-size    :int32
    :record-table-entries :int32
    :string-table-start   :int32
-   :string-table-size    :int32])
+   :string-table-size    :int32))
+
+(def arz-string-table
+  (variable-count
+   (string :ascii)
+   :length-prefix :int32))
 
 ;; (gloss/defcodec arz-record-header-codec
 ;;   (gloss/ordered-map
@@ -111,7 +301,12 @@
 
   ;; Jump to the start of the string table and start decoding
   (.position bb (:string-table-start header))
+  (read-struct arz-string-table bb))
 
+(defn- load-db-string-table2
+  [^java.nio.ByteBuffer bb header]
+
+  (.position bb (:string-table-start header))
   ;; How many strings do we have?
   (let [str-count (.getInt bb)]
 
@@ -138,7 +333,6 @@
              ;; terminate by returning the string table
              (recur (inc i) limit (conj string-table (String. str-buffer)))))))))
 
-
 (defn load-game-db
   [filepath]
 
@@ -164,6 +358,5 @@
 #_(def f (mmap "/Users/Odie/Dropbox/Public/GrimDawn/database/database.arz"))
 #_(.order f java.nio.ByteOrder/LITTLE_ENDIAN)
 #_(def h (load-db-header f))
-#_(.position f (:string-table-start h))
-#_(gloss.io/decode arz-string-table-codec f false)
-#_(gloss.io/encode arz-string-table-codec ["hello" "world"])
+#_(def st (load-db-string-table f h))
+#_(def st (load-db-string-table2 f h))
