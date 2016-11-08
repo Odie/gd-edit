6f0426484933dfe7778371d03e36ec3a692923c7
Jonathan Shieh <jonathan.shieh@gmail.com>
2016-11-08 12:18:36 +0800

    Refactors read- functions so they can be called manually

--- src/gd_edit/gdc_reader.clj
+++ src/gd_edit/gdc_reader.clj
@@ -49,7 +49,6 @@
   "Given a byte array, the current encryption state, and an encryption table, return the next enc state"
   [byte-data enc-state enc-table]
 
-  (println byte-data)
   (bit-xor enc-state (enc-table (Byte/toUnsignedInt byte-data))))
 
 (defn enc-next-state-with-byte-array
@@ -79,10 +78,8 @@
 
     (enc-next-state-with-byte byte-data enc-state enc-table)))
 
-(defn transform-bytes!
-  "Decrypts the given byte array in place while updating the encryption context"
+(defn decrypt-bytes!
   [buffer context]
-
   (loop [i 0
          limit (count buffer)
          enc-state (:enc-state context)
@@ -91,34 +88,44 @@
 
     ;; Finished transforming the byte array?
     (if (>= i limit)
-      (do
-        ;; Update the context with the new state
-        (reset! context (assoc  @context :enc-state enc-state))
-
-        ;; Also return the new enc-state
+        ;; Return the new enc-state
         enc-state
-        )
 
 
+      ;; Not done?
+      ;; Grab the next byte, decrypt it, store it back in the same spot
       (let [enc-val (aget buffer i)
             val (byte (bit-and 0x00000000000000ff (bit-xor enc-val enc-state)))]
 
         (aset buffer i val)
         (recur (inc i) limit (enc-next-state enc-val enc-state enc-table) enc-table)
-        )
+        ))))
 
-      )
-    )
-  )
+(defn transform-bytes!
+  "Decrypts the given byte array in place while updating the encryption context"
+  [buffer context-atom]
 
-(defn read-byte
+  (let [next-enc-state (decrypt-bytes! buffer @context-atom)]
+
+    ;; Update the context with the new state
+    (reset! context-atom (assoc  @context-atom :enc-state next-enc-state))))
+
+(defn- read-bytes-
+  [^ByteBuffer bb {:keys [enc-state enc-table] :as context} byte-count]
+
+  (let [buffer (byte-array byte-count)]
+    (.get bb buffer 0 byte-count)
+
+    [buffer (decrypt-bytes! buffer context)]))
+
+(defn- read-byte-
   "Retrieve the next byte, decrypt the value, then return the value and the next enc state"
   [^ByteBuffer bb {:keys [enc-state enc-table] :as context}]
 
   (let [enc-val (.get bb)]
     [(byte (bit-and 0x00000000000000ff (bit-xor enc-val enc-state))) (enc-next-state enc-val enc-state enc-table)]))
 
-(defn read-int
+(defn- read-int-
   "Retrieve the next byte, decrypt the value, then return the value and the next enc state"
   [^ByteBuffer bb {:keys [enc-state enc-table] :as context}]
 
@@ -132,13 +139,13 @@
     ;; Return [decrypted-value next-enc-state] pair
     [(bit-and 0x00000000ffffffff (bit-xor enc-val enc-state)) (enc-next-state byte-data enc-state enc-table)]))
 
-(defn read-bool
+(defn- read-bool-
   [^ByteBuffer bb {:keys [enc-state enc-table] :as context}]
 
   (let [[val next-enc-state] (read-byte bb context)]
     [(= val 1) next-enc-state]))
 
-(defn read-float
+(defn- read-float-
   "Retrieve the next byte, decrypt the value, then return the value and the next enc state"
   [^ByteBuffer bb {:keys [enc-state enc-table] :as context}]
 
@@ -149,11 +156,16 @@
 
     [val (enc-next-state byte-data enc-state enc-table)]))
 
+(def read-bytes (partial read-and-update-context read-bytes-))
+(def read-bool (partial read-and-update-context read-bool-))
+(def read-int (partial read-and-update-context read-int-))
+(def read-float (partial read-and-update-context read-float-))
+
 (defn read-and-update-context
-  [read-fn ^ByteBuffer bb context]
+  [read-fn ^ByteBuffer bb context & rest]
 
   ;; Run the read function
-  (let [[val new-enc-state] (read-fn bb @context)]
+  (let [[val new-enc-state] (apply read-fn bb @context rest)]
 
     ;; Update the enc-state
     (reset! context (assoc @context :enc-state new-enc-state))
@@ -170,13 +182,11 @@
          :enc-table enc-table
 
          ;; Specs table for the structure lib
-         :primitive-specs {:transform (fn [buffer context]
-                                        ()
-                                        )
-                           :byte   [:byte    1 (partial read-and-update-context read-byte) ]
-                           :bool   [:byte    1 (partial read-and-update-context read-bool) ]
-                           :int32  [:int32   4 (partial read-and-update-context read-int)  ]
-                           :float  [:float   4 (partial read-and-update-context read-float)]}
+         :primitive-specs {:transform-bytes! transform-bytes!
+                           :byte   [:byte    1 read-byte ]
+                           :bool   [:byte    1 read-bool ]
+                           :int32  [:int32   4 read-int  ]
+                           :float  [:float   4 read-float]}
          }))
 
 (defn generate-encryption-table
@@ -235,8 +245,16 @@
 
 
         header (s/read-struct Header bb enc-context)
-        ]
 
-    header))
+        header-checksum (Integer/toUnsignedLong (.getInt bb))
+        _ (assert (= header-checksum (:enc-state @enc-context)))
+
+        data-version (read-int bb enc-context)
+        _ (if (not (contains? #{6 7} data-version))
+            (throw (Throwable. "I can't read this gdc format!")))
+
+        mystery-field (read-bytes bb enc-context 16)]
+
+    mystery-field))
 
 #_(def r (load-character-file "/Users/Odie/Dropbox/Public/GrimDawn/main/_Hetzer/player.gdc"))
