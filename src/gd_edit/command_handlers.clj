721f95635b279bcec93a1a98e1e2079f20fbd076
Jonathan Shieh <jonathan.shieh@gmail.com>
2017-01-02 23:36:04 +0800

    Newly added class gets mastery/level 1

--- src/gd_edit/command_handlers.clj
+++ src/gd_edit/command_handlers.clj
@@ -1576,7 +1576,7 @@
                                         :autocast-skill-name ""
                                         :skill-transition false
                                         :skill-name (:recordname klass),
-                                        :level (:character-level @globals/character)
+                                        :level 1
                                         :sublevel 0
                                         :autocast-controller-name ""
                                         :enabled true}
