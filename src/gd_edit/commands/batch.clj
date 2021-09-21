da1c6a0d6df2b97cc3b3014f2e941f304b458c92
Jonathan Shieh <jonathan.shieh@gmail.com>
2021-09-21 13:13:33 +0800

    Adds ability to stop "batch character" using ctrl-c

--- src/gd_edit/commands/batch.clj
+++ src/gd_edit/commands/batch.clj
@@ -6,7 +6,8 @@
             [gd-edit.commands.write :as write]
             [gd-edit.utils :as u]
             [gd-edit.progress-bar :as progress]
-            ))
+            )
+  (:import [sun.misc Signal SignalHandler]))
 
 (defn batch-handler
   [[_ tokens]]
@@ -24,10 +25,42 @@
         (u/print-line)
         (u/print-line (green "Batch file completed!"))))))
 
+(defn setup-INT-handler
+  [context-a]
+
+  ;; Sanity check
+  ;; Do not setup the handler multiple times
+  (when-not (:cur-handler @context-a)
+    (let [;; Create a new handler for the INT signal
+          new-handler (proxy [SignalHandler] []
+                        (handle [sig]
+                          ;; When the signal is detected...
+                          ;; Set a flag in the context atom
+                          ;; We're going to pick up the signal elsewhere
+                          ;; in the code
+                          (swap! context-a assoc :INT-detected true)
+                          (println "\nStopping batch character...")))
+
+          ;; Hook in the new handler
+          ;; Store away the old handler so it can be restored later
+          old-handler (Signal/handle (Signal. "INT") new-handler)]
+
+      ;; Store both handlers to be referenced later
+      (reset! context-a
+              {:cur-handler new-handler
+               :old-handler old-handler}))))
+
+(defn teardown-INT-handler
+  [context-a]
+  (when (:old-handler @context-a)
+    ;; Restore the old signal handler
+    (Signal/handle (Signal. "INT") (:old-handler @context-a))))
+
 (defn batch-character-handler
   [[_ tokens]]
 
   (let [batch-file (io/file (first tokens))
+        verbose? (u/case-insensitive= (second tokens) "verbose")
         batch-repl (find-var 'gd-edit.core/batch-repl)]
     (if (not (.exists batch-file))
       (u/print-line (format "%s:\n\t%s" (red "Cannot locate the batch file") (yellow (.getCanonicalPath batch-file))))
@@ -36,20 +69,47 @@
                             (slurp)
                             (str/split-lines))
             char-locs (choose-character/character-list)
+            handler-context (atom {})
+            _ (setup-INT-handler handler-context)
+
+            completion-count (atom 0)
+
             start-time (System/nanoTime)
+
             char-count (count char-locs)]
-        (binding [u/*suppress-print* true]
-          (doseq [[idx char-loc] (u/indexed char-locs)]
-          ;; (doseq [char-loc char-locs]
-            ;; (progress/display-file-progress (inc idx) char-count)
-            (progress/display-progress (inc idx) char-count)
-            (print "\r")
-            ;; (print ".")
-            (choose-character/load-character-file (:gdc-path char-loc))
-            (batch-repl batch-cmds)
-            (write/write-loaded-character-file!)
-            (u/print-line)))
+
+        ;; Optionally turn off all prints while executing the batch file
+        (binding [u/*suppress-print* (not verbose?)]
+          ;; Loop over each of the known characters
+          (loop [char-list (u/indexed char-locs)]
+            (cond
+              ;; Stop when the list is exhausted or...
+              ;; when the user wants to cancel/stop the batch process
+              (not char-list) :ok
+              (:INT-detected @handler-context) :interrupted
+
+              :else
+              (let [[idx char-loc] (first char-list)]
+                ;; Optionally show the progress bar
+                (when (not verbose?)
+                  (progress/display-progress (inc idx) char-count))
+
+                ;; Load up the character file
+                (choose-character/load-character-file (:gdc-path char-loc))
+
+                ;; Run the batch commands
+                (batch-repl batch-cmds)
+
+                ;; Write out the character file
+                (write/write-loaded-character-file!)
+                (u/print-line)
+                (swap! completion-count inc)
+
+                ;; Proceed with the next character
+                (recur (next char-list))))))
+
+        (teardown-INT-handler handler-context)
 
         (u/newline-)
-        (u/print-line (format (green "Batch file completed for %d characters!") (count char-locs)))
+        (u/print-line (format (green "Batch file completed for %d characters!") @completion-count))
         (u/print-line (format (green "Elapsed: %f seconds") (u/nanotime->secs (- (System/nanoTime) start-time))))))))
